d99a33808f7a99eb9d22241954f4e6402c877c5b
Alan Lima <alan.lima@hartbgroup.com.br>
2021-03-08 12:15:07 -0300

    Initial Commit

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Alan Lima on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
